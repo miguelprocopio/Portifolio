2810cd1517d477d12aaf46e8173f3d1fe5721e97
Miguel Procópio <guelprocopio@gmail.com>
2025-04-11 14:37:17 -0300

    feat: adiciona navegação principal com React Router e páginas iniciais.

--- Portifolio/src/App.jsx
+++ Portifolio/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Inicio from './paginas/inicio';
+import Sobre from './paginas/Sobre';
+import Projetos from './paginas/Projetos';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="app-container">
+        <h1>Meu Portfólio</h1>
+
+        <nav className="nav">
+          <Link to="/"><button>Início</button></Link>
+          <Link to="/sobre"><button>Sobre Mim</button></Link>
+          <Link to="/projetos"><button>Projetos</button></Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/projetos" element={<Projetos />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
