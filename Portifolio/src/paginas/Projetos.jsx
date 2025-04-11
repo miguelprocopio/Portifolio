4afd75ab9e066af013ba6722f0e1b7ec0e776129
Miguel Procópio <guelprocopio@gmail.com>
2025-04-11 15:51:33 -0300

    feat: adiciona target='_blank' e rel='nofollow' para abrir links externos em nova aba.

--- Portifolio/src/paginas/Projetos.jsx
+++ Portifolio/src/paginas/Projetos.jsx
@@ -3,9 +3,9 @@ function Projetos() {
       <div className="card">
         <h2>Projetos</h2>
         <ul>
-          <li><a href="https://github.com/miguelprocopio/Exercicios-PWM-2025.git">Projeto 1</a> - Minha Rotina e Cadastro de Usuário</li>
-          <li><a href="https://github.com/pedromota001/Projeto-Banco-de-Dados.git">Projeto 2</a> - Projeto de Banco de Dados</li>
-          <li><a href="https://github.com/c3-disciplina-ed2/projeto-jogo-wake-up-mr-west.git">Projeto 3</a> - Jogo Wake Up Mr. West</li>
+          <li><a href="https://github.com/miguelprocopio/Exercicios-PWM-2025.git" target="_blank" rel="nofollow">Projeto 1</a> - Minha Rotina e Cadastro de Usuário</li>
+          <li><a href="https://github.com/pedromota001/Projeto-Banco-de-Dados.git" target="_blank" rel="nofollow">Projeto 2</a> - Projeto de Banco de Dados</li>
+          <li><a href="https://github.com/c3-disciplina-ed2/projeto-jogo-wake-up-mr-west.git" target="_blank" rel="nofollow">Projeto 3</a> - Jogo Wake Up Mr. West</li>
         </ul>
       </div>
     );
